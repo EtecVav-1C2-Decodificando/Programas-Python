eb6e6187eb7ed1e6f6a641547eaa8a45d90a7cc6
WilliamLucas835 <Williamlucasd04@gmail.com>
2025-11-07 21:48:33 -0300

    Add files via upload

--- Lista Códigos (06-11)/Programa13/Programa13.py
+++ Lista Códigos (06-11)/Programa13/Programa13.py
@@ -1,1 +1,13 @@
+r = "s"
 
+while r == 's':
+	
+	def media (v):
+		return sum(v)/len(v)
+		
+	lista = list(map(int, input(f"Digite a lista separada por espaços: ").split()))
+	
+	print("A media é: ", media(lista))
+	
+	r = input("Deseja continar (s/n)?: ")
+	
